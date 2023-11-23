bc8e92d06376b516419774624318c9126d382000
vykuntarao tangudu <vykuntoasis@gmail.com>
2023-11-23 00:32:03 +0000

    Initial Commit @11/23/2023, 12:32:03 AM

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("anuabhavalp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
